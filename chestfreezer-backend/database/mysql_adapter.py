9ece3ac3372eb571eba87d809ce1686d026c04ef
Theoklitos Christodoulou <thechristodoulou@gmail.com>
2014-04-05 04:43:32 +0200

    added another str() conversion

--- chestfreezer-backend/database/mysql_adapter.py
+++ chestfreezer-backend/database/mysql_adapter.py
@@ -44,7 +44,7 @@ def store_probe(probe, should_overwrite=True):
     """ stores (with the option to overwrite) a new probe """    
     cursor.execute("SELECT * FROM " + PROBES_TABLE_NAME + " WHERE probe_id='" + str(probe.probe_id) + "'")
     results = cursor.fetchall()
-    sql_statement = "INSERT INTO " + PROBES_TABLE_NAME + " VALUES ('" + probe.probe_id + "','" + probe.name + "', FALSE)"
+    sql_statement = "INSERT INTO " + PROBES_TABLE_NAME + " VALUES ('" + str(probe.probe_id) + "','" + probe.name + "', FALSE)"
     if len(results) == 0:
         cursor.execute(sql_statement)
         print 'Registered new probe #' + probe.probe_id
