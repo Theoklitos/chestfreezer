96c1161e92e966705590cfec9b4a1756405d2be2
Theoklitos Christodoulou <thechristodoulou@gmail.com>
2014-05-11 21:46:59 +0200

    fixed same origin policy IP problem

--- chestfreezer-frontend/scripts/configuration.js
+++ chestfreezer-frontend/scripts/configuration.js
@@ -2,7 +2,7 @@
  * Configuration for the chestfreezer app
  */
 define({
-	rootUrl : 'http://localhost:8080/chestfreezer/api',
+	rootUrl : '0.0.0.0:8080/chestfreezer/api',
 	temperatureUpdateIntervalSeconds : 3,
 	instructionUpdateIntervalSeconds : 60,
 	showLoginForm : false,
